bfc5d834bf60e4a7f113368b8f67979193fda3d6
Luisag12 <86633888+Luisag12@users.noreply.github.com>
2021-09-10 17:31:34 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () =>(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-md">
-            <a className="navbar-brand" href="#">Personajes Shadow Hunters</a>
+            <a className="navbar-brand" href="">Personajes Shadow Hunters</a>
         </div>
     </nav>
 )
